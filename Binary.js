faa72899f64e24b0c2d6bf99b85008ae9692a255
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-09-20 22:11:56 -0500

    Create Binary.js
    AHHHHHHHHHHHHHHHH

---
+++ Binary.js
@@ -0,0 +1,19 @@
+function(c1r, c2r, c3r, c4r, c5r, c6r, c7r, c8r, c9r, c10r, c11r, c12r, c13r, c14r, c15r, c16r, c17r, c18r, c19r, c20r, c21r, c22r, c23r, c24r, c25r, c26r, c27r, c28r, c29r, c30r, c31r, c32r, c33r, c34r, c35r, c36r, c37r, c38r, c39r, c40r, c41r, c42r, c43r, c44r, c45r, c46r, c47r, c48r, c49r, c50r, c51r, c52r, c53r, c54r, c55r, c56r, c57r, c58r, c59r, c60r, c61r, c62r, c63r, c64r){
+  let or = 111111111010010100000000;
+  let db = 000000000000001111111111;
+  let dbo = 000000000000001010011001;
+  let pi = 111111110000000010101011;
+  let pio = 101101010000000010101100;
+  let y = 111111111111101100000000;
+  let yo = 101100001011111100000000;
+  let b = 000000001111111111100111;
+  let bo = 000000001101110011000111;
+  let g = 000000011111111100000000;
+  let go = 000000011100011000000000;
+  let pu = 101000000000000011111111;
+  let puo = 011110010000000011000001;
+  let w = 111111111111111111111111;
+  let bl = 000000000000000000000000;
+  return c1r c2r c3r c4r c5r c6r c7r c8r c9r c10r c11r c12r c13r c14r c15r c16r c17r c18r c19r c20r c21r c22r c23r c24r c25r c26r c27r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c38r c39r c40r c41r c42r c43r c44r c45r c46r c47r c48r c49r c50r c51r c52r c53r c54r c55r c56r c57r c58r c59r c60r c61r c62r c63r c64r;
+};
+//dows it work? idfk this is due monday so it better
