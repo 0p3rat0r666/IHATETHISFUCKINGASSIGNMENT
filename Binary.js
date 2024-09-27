50fe7fef70e2e5e3388c78fc28f215003d675cde
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-09-27 17:26:09 -0500

    Update Binary.js

--- Binary.js
+++ Binary.js
@@ -1,4 +1,4 @@
-function(c1r, c2r, c3r, c4r, c5r, c6r, c7r, c8r, c9r, c10r, c11r, c12r, c13r, c14r, c15r, c16r, c17r, c18r, c19r, c20r, c21r, c22r, c23r, c24r, c25r, c26r, c27r, c28r, c29r, c30r, c31r, c32r, c33r, c34r, c35r, c36r, c37r, c38r, c39r, c40r, c41r, c42r, c43r, c44r, c45r, c46r, c47r, c48r, c49r, c50r, c51r, c52r, c53r, c54r, c55r, c56r, c57r, c58r, c59r, c60r, c61r, c62r, c63r, c64r){
+function rows(c1r, c2r, c3r, c4r, c5r, c6r, c7r, c8r, c9r, c10r, c11r, c12r, c13r, c14r, c15r, c16r, c17r, c18r, c19r, c20r, c21r, c22r, c23r, c24r, c25r, c26r, c27r, c28r, c29r, c30r, c31r, c32r, c33r, c34r, c35r, c36r, c37r, c38r, c39r, c40r, c41r, c42r, c43r, c44r, c45r, c46r, c47r, c48r, c49r, c50r, c51r, c52r, c53r, c54r, c55r, c56r, c57r, c58r, c59r, c60r, c61r, c62r, c63r, c64r){
   let or = 111111111010010100000000;
   let db = 000000000000001111111111;
   let dbo = 000000000000001010011001;
@@ -14,6 +14,7 @@ function(c1r, c2r, c3r, c4r, c5r, c6r, c7r, c8r, c9r, c10r, c11r, c12r, c13r, c1
   let puo = 011110010000000011000001;
   let w = 111111111111111111111111;
   let bl = 000000000000000000000000;
-  return c1r c2r c3r c4r c5r c6r c7r c8r c9r c10r c11r c12r c13r c14r c15r c16r c17r c18r c19r c20r c21r c22r c23r c24r c25r c26r c27r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c38r c39r c40r c41r c42r c43r c44r c45r c46r c47r c48r c49r c50r c51r c52r c53r c54r c55r c56r c57r c58r c59r c60r c61r c62r c63r c64r;
+  let results = c1r c2r c3r c4r c5r c6r c7r c8r c9r c10r c11r c12r c13r c14r c15r c16r c17r c18r c19r c20r c21r c22r c23r c24r c25r c26r c27r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c28r c29r c30r c31r c32r c33r c34r c35r c36r c37r c38r c39r c40r c41r c42r c43r c44r c45r c46r c47r c48r c49r c50r c51r c52r c53r c54r c55r c56r c57r c58r c59r c60r c61r c62r c63r c64r;
+	return results;
 };
 //dows it work? idfk this is due monday so it better
